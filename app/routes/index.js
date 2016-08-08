0844dd322b93726b2daf3ed534927074206ec594
Alex Blom <alex@alexblom.com>
2016-08-08 13:53:03 -0400

    feat(geo): example with working geo

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  cordova: Ember.inject.service(),
+
   model() {
     return this.get('store').findAll('rental');
+  },
+
+  afterModel() {
+    this.get('cordova').on('deviceready', this, '_checkGeoLocation');
+  },
+
+  _checkGeoLocation() {
+    navigator.geolocation.getCurrentPosition(
+      function(position) {
+        alert(position.coords);
+      },
+      function(error) {
+        alert(error);
+      }
+    );
   }
 });
